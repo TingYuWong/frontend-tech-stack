d43878b62383d5d74145905b7341f40c0fbc52fb
tinawong <tina.ty.wong@viewsonic.com>
2023-11-17 07:58:46 +0800

    fix: yarn lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,39 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,jsx,cjs,ts,tsx}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+  },
+};
