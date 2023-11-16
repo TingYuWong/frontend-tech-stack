f6f8c2291385d471df31ebe42f372ef967823706
tinawong <tina.ty.wong@viewsonic.com>
2023-11-16 09:02:59 +0800

    feat: styled-component coding style

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,12 @@
+import { Wrapper, Title, Content } from "./style";
+
+const Playground = () => {
+  return (
+    <Wrapper>
+      <Title>Styled Component</Title>
+      <Content>Content</Content>
+    </Wrapper>
+  );
+};
+
+export default Playground;
---
+++ src/components/Playground/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 30px;
+  border: 2px solid lightseagreen;
+  border-radius: 8px;
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+export const Content = styled.div`
+  font-size: 20px;
+  font-weight: normal;
+`;
