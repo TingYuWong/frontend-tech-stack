db30414fdd58a0bc11b492f0ba4e2adfe1b5383b
tinawong <tina.ty.wong@viewsonic.com>
2023-11-16 09:48:02 +0800

    feat: add playground component

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.css";
+import CustomInput from "@/components/ant/CustomInput/CustomInput";
+
+const Playground = () => {
+  return (
+    <div className={styles.wrapper}>
+      <CustomInput />
+    </div>
+  );
+};
+
+export default Playground;
