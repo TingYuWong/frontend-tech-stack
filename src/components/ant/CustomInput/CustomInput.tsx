23139c90a19a6df71b885eb3b05089532748c30c
tinawong <tina.ty.wong@viewsonic.com>
2023-11-16 09:48:11 +0800

    feat: add custom input component

---
+++ src/components/ant/CustomInput/CustomInput.tsx
@@ -0,0 +1,15 @@
+import { Input } from "antd";
+
+const CustomInput: React.FC = () => (
+  <Input
+    placeholder="Basic usage"
+    style={{
+      height: "80px",
+      borderRadius: "8px",
+      fontSize: "30px",
+      fontWeight: 600,
+    }}
+  />
+);
+
+export default CustomInput;
