ff0fd33c2cb76fd3e657c2a8bd8954ad24ffd347
tinawong <tina.ty.wong@viewsonic.com>
2023-11-20 11:16:56 +0800

    feat: add url detection test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,6 +4,7 @@ describe("Home Page", () => {
   it("Title should be 'Home'", () => {
     cy.visit("http://localhost:5173");
     cy.contains("Click").click();
-    cy.contains("About");
+    // cy.contains("About");
+    cy.url().should("include", "/about");
   });
 });
