2eb83826ac5f4c42a783b7129f3fe6705b21b6e0
tinawong <tina.ty.wong@viewsonic.com>
2023-11-20 11:15:52 +0800

    feat: add button click test

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe("Home Page", () => {
+  it("Title should be 'Home'", () => {
+    cy.visit("http://localhost:5173");
+    cy.contains("Click").click();
+    cy.contains("About");
+  });
+});
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const toAboutPage = () => {
+    navigate("/about");
+  };
+
+  return (
+    <div>
+      <div>Home</div>
+      <button onClick={toAboutPage}>Click</button>
+    </div>
+  );
 };
 
 export default Home;
