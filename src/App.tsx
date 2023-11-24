e5ab9ab13e7889dac6ce262a5c701b0a5e89b382
tinawong <tina.ty.wong@viewsonic.com>
2023-11-24 12:04:34 +0800

    feat: useReducer basic usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+// state
+type AppState = {
+  age: number;
+  name: string;
+};
+
+// action
+type IncrementAge = {
+  type: "increment_age";
+  payload: number;
+};
+type DecrementAge = {
+  type: "decrement_age";
+  payload: number;
+};
+type ChangeName = {
+  type: "change_name";
+  payload: string;
+};
+type AppActions = IncrementAge | DecrementAge | ChangeName;
+
+// reducer
+const appReducer = (state: AppState, action: AppActions) => {
+  switch (action.type) {
+    case "increment_age":
+      return { ...state, age: state.age + action.payload };
+    case "decrement_age":
+      return { ...state, age: state.age - action.payload };
+    case "change_name":
+      return { ...state, name: action.payload };
+  }
+};
+
+const initialState = { age: 0, name: "Tina" };
+
+const App = () => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div onClick={() => dispatch({ type: "increment_age", payload: 1 })}>
+        + age
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div onClick={() => dispatch({ type: "decrement_age", payload: 1 })}>
+        - age
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        value={state.name}
+        onChange={(e) =>
+          dispatch({ type: "change_name", payload: e.target.value })
+        }
+      />
+      <div>age: {state.age}</div>
+      <div>name: {state.name}</div>
     </>
   );
-}
+};
 
 export default App;
